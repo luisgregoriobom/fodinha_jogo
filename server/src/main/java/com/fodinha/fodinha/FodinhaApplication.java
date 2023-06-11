511dbad0259fdca148041b9be6d23fcd31714ca5
Luis Gregorio <gregoriobom@hotmail.com>
2023-06-11 02:38:11 -0300

    initial commit

---
+++ server/src/main/java/com/fodinha/fodinha/FodinhaApplication.java
@@ -0,0 +1,12 @@
+package com.fodinha.fodinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FodinhaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FodinhaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/fodinha/fodinha/FodinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fodinha.fodinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FodinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
